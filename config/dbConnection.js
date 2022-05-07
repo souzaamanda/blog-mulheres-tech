e81b96b6a41380ae9653ce356609bd9c9d368f94
Amanda Souza <amanda_souza@live.com>
2022-05-06 22:57:26 -0300

    configurações para o deploy PostGreSQL

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,7 +1,7 @@
 const {Client} = require('pg')
 
 const client = new Client({
-    connectionString:'postgres://rhsjrivjuwxnya:f7735bccdfb235b737cf68fa98beb4d88317446ee938f720f81b12f364f3a3cf@ec2-54-158-247-210.compute-1.amazonaws.com:5432/d1tnuicb8gu2ml',
+    connectionString: process.env.DATABASE_URL || 'postgres://rhsjrivjuwxnya:f7735bccdfb235b737cf68fa98beb4d88317446ee938f720f81b12f364f3a3cf@ec2-54-158-247-210.compute-1.amazonaws.com:5432/d1tnuicb8gu2ml',
     ssl:{rejectUnauthorized: false}
 })
 
